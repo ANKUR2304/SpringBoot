a7abf0850faf5ae701a9880fe6d6bec381d84f61
ANKUR2304 <jakharankur5@gmail.com>
2024-06-30 01:51:36 +0530

    Added - Simple statement using JDBC

--- 1. JDBC/JDBC/src/main/java/org/example/Main.java
+++ 1. JDBC/JDBC/src/main/java/org/example/Main.java
@@ -8,16 +8,18 @@ public class Main {
         String username = "your_username"; // Database username
         String password = "your_password"; // Database password
 
-        String insertSQL = "INSERT INTO test (id, username, currorg) VALUES (?, ?, ?)";
+        //String insertSQL = "INSERT INTO test (id, username, currorg) VALUES (?, ?, ?)";
 
         Connection connection = null;
-        PreparedStatement preparedStatement = null;
         try{
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(url, username, password);
 
+            System.out.println("Connection Established!");
+
+            /*
             // Create a prepared statement
-            preparedStatement = connection.prepareStatement(insertSQL);
+            PreparedStatement preparedStatement = connection.prepareStatement(insertSQL);
 
             // Set the parameters
             preparedStatement.setInt(1, 1); // ID
@@ -28,6 +30,23 @@ public class Main {
             int rowsInserted = preparedStatement.executeUpdate();
             if (rowsInserted > 0) {
                 System.out.println("A new user was inserted successfully!");
+            }
+            */
+
+            // Create a simple statement
+            Statement statement = connection.createStatement();
+
+            // Execute a query
+            String query = "SELECT * FROM test";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            // Process the results
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("username");
+                String currOrg = resultSet.getString("currorg");
+
+                System.out.println("ID: " + id + ", Name: " + name + ", Org: " + currOrg);
             }
         }
         catch (SQLException e){
